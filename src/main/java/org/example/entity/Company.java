84d0d0e9cbc7eaf4682188c60d184ad152d8df27
“antoanparashkevov” <tonkata1505@gmail.com>
2024-01-06 11:02:54 +0200

    Create an entity (model) mapped to the company table

--- src/main/java/org/example/entity/Company.java
+++ src/main/java/org/example/entity/Company.java
@@ -1,14 +1,19 @@
 package org.example.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
+@Table(name = "company")
 public class Company {
 
     @Id//to tell that this should be the primary key, add this
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
+
+    private LocalDate foundationDate;
 
 }
