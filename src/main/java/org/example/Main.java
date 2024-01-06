0830e289ed6560e3c6f05c3b16dd6134e3194299
“antoanparashkevov” <tonkata1505@gmail.com>
2024-01-06 10:21:21 +0200

    Remove the unnecessary model and configuration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,8 @@
 package org.example;
 
-//import org.example.configuration.SessionFactoryUtil;
-//import org.hibernate.Session;
-
 public class Main {
     public static void main(String[] args) {
-//        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()){
-//
-//        }
 
         System.out.println("Hello world");
-
     }
 }
