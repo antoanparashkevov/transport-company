a7372ef6e63186f41fe242d217bec52a4fcfa651
“antoanparashkevov” <tonkata1505@gmail.com>
2024-01-06 12:28:27 +0200

    Create CompanyDao and create a mechanism for inserting new records into the company table

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,18 @@ package org.example;
 
 import org.example.configuration.SessionFactoryUtil;
 
+import org.example.dao.CompanyDao;
+import org.example.entity.Company;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
 
         SessionFactoryUtil.getSessionFactory().openSession();
+
+        Company company = new Company();
+
+        CompanyDao.createCompany(company);//create
     }
 }
--- src/main/java/org/example/dao/CompanyDao.java
+++ src/main/java/org/example/dao/CompanyDao.java
@@ -1,4 +1,26 @@
 package org.example.dao;
 
+//DAO - Data Access object
+
+//DAO layer
+//there is no need to add any annotations since we get the session from the session configuration in order to communicate with the database
+
+import org.example.configuration.SessionFactoryUtil;
+import org.example.entity.Company;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class CompanyDao {
+
+    public static void createCompany(Company company) {
+
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            session.save(company);
+
+            transaction.commit();
+        }
+    }
+
 }
