a9efd19780ead93ca63606798705094752db904f
“antoanparashkevov” <tonkata1505@gmail.com>
2024-01-06 11:03:16 +0200

    Open a session using the session configuration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import org.example.configuration.SessionFactoryUtil;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+
+        SessionFactoryUtil.getSessionFactory().openSession();
     }
 }
