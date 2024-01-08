5b80f0bdfb1b7e1214d5b3f751020c68438f722d
“antoanparashkevov” <tonkata1505@gmail.com>
2024-01-08 14:06:26 +0200

    Create CompanyEmployeesDto

--- src/main/java/org/example/dto/CompanyDto.java
+++ src/main/java/org/example/dto/CompanyDto.java
@@ -1,4 +1,5 @@
 package org.example.dto;
 
 public class CompanyDto {
+
 }
--- src/main/java/org/example/dto/CompanyEmployeesDto.java
+++ src/main/java/org/example/dto/CompanyEmployeesDto.java
@@ -1,2 +1,33 @@
-package org.example.dto;public class CompanyEmployeesDto {
+package org.example.dto;
+
+public class CompanyEmployeesDto {
+
+    private long companyId;
+    private String companyName;
+
+    private long employeeId;
+    private String employeeName;
+
+    public CompanyEmployeesDto(long companyId, String companyName, long employeeId, String employeeName) {
+        this.companyId = companyId;
+        this.companyName = companyName;
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+    }
+
+    public long getCompanyId() {
+        return companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
 }
