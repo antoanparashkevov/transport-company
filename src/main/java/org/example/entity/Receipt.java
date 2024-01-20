42a8d038fa5129fa3447ce5d54a1213d86d0a388
“antoanparashkevov” <tonkata1505@gmail.com>
2024-01-20 15:45:10 +0200

    Add comments and validation constraints to the Receipt entity

--- src/main/java/org/example/entity/Receipt.java
+++ src/main/java/org/example/entity/Receipt.java
@@ -2,19 +2,21 @@ package org.example.entity;
 
 import jakarta.persistence.*;
 
-@Entity
-@Table(name="receipt")
+@Entity//mandatory annotation to be able to connect this class with the corresponding table
+@Table(name="receipt")//overwrite the auto generated table name
 public class Receipt {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id//This annotation tells that the field below should be the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Auto Increment (AI) property
     private long id;
 
+    //implement lazy loading when read a company
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Client client;
+
+    //implement lazy loading when read a company
     @OneToOne(optional = false, fetch = FetchType.LAZY)
     private Purchase purchase;
-
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)//not nullable
-    private Client client;
 
     //default constructor
     public Receipt() {
@@ -26,12 +28,28 @@ public class Receipt {
         return id;
     }
 
+    public Client getClient() {
+        return client;
+    }
+
+    public Purchase getPurchase() {
+        return purchase;
+    }
+
     //GETTERS END
 
     //SETTERS START
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public void setPurchase(Purchase purchase) {
+        this.purchase = purchase;
     }
 
     //SETTERS END
