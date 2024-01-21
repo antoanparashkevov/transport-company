84635b16075f0b8d017250117b780816a2977d83
“antoanparashkevov” <tonkata1505@gmail.com>
2024-01-21 20:15:28 +0200

    Add new field named unit inside the Truck entity

--- src/main/java/org/example/entity/Truck.java
+++ src/main/java/org/example/entity/Truck.java
@@ -1,12 +1,15 @@
 package org.example.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.example.entity.enumeration.CapacityUnit;
 
 @Entity
 @Table(name="truck")
 public class Truck extends Vehicle {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "unit", nullable = false)
+    private CapacityUnit unit;
 
     @Column(name="capacity", nullable = false)
     private float capacity;
@@ -15,22 +18,40 @@ public class Truck extends Vehicle {
     public Truck() {}
 
     //parametrized constructor
-    public Truck(String registrationNumber, Company company, float capacity) {
+    public Truck(String registrationNumber, Company company, CapacityUnit unit, float capacity) {
         super(registrationNumber, company);
+        this.unit = unit;
         this.capacity = capacity;
+    }
+
+    //GETTERS START
+
+    public CapacityUnit getUnit() {
+        return unit;
     }
 
     public float getCapacity() {
         return capacity;
     }
 
+    //GETTERS END
+
+    //SETTERS START
+
+    public void setUnit(CapacityUnit unit) {
+        this.unit = unit;
+    }
+
     public void setCapacity(float capacity) {
         this.capacity = capacity;
     }
 
+    //SETTERS END
+
     @Override
     public String toString() {
         return "Truck{" +
+                "unit=" + unit +
                 "capacity=" + capacity +
                 "} " + super.toString();
     }
